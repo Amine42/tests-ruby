223b461803ce0de4617c8302c24e4d774b24f738
Amine42 <amine.bouraghda@gmail.com>
2019-01-14 23:51:20 +0100

    delete the two firs letter

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -3,13 +3,29 @@ def add_ay(array)
 end
 
 def move_two_first_char(array)
+  # if ()
 
+  # puts array
+  real_word = []
+  non_vowel = []
+  array.map.with_index do |word, index|
+    if word[0].upcase != 'A' && word[0].upcase != 'E' && word[0].upcase != 'I' && word[0].upcase != 'O' && word[0].upcase != 'u' && word[0].upcase != 'y'
+      non_vowel[index] = word[0..1]
+      real_word[index] = word.delete(word[0..1]) + non_vowel[index]
+    else
+      real_word[index] = word
+    end
+  end
+  real_word
 
+  # non_vowel_compact = non_vowel.compact!
+  # puts non_vowel_compact
 end
 
 def translate(str)
   word = str.split
-  # puts add_ay(word)
+  
+  add_ay(move_two_first_char(word))
 end
 
-puts translate("apple tomate")
+puts translate("the apple tomate the test")
