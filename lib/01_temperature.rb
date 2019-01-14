6977f4d42afdb1f21355f60a34bcf9bb8e598eee
Amine42 <amine.bouraghda@gmail.com>
2019-01-14 12:57:16 +0100

    all test is ok for 01_temperature.rb

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -1,7 +1,7 @@
 def ftoc(fahrenheit)
-  celsius = (fahrenheit - 32) / 1.8
+  celsius = ((fahrenheit - 32) / 1.8).round(2)
 end
 
 def ctof(celsius)
-  fahrenheit = (celsius * 1.8) + 32
+  fahrenheit = ((celsius * 1.8) + 32).round(2)
 end
