54fff4e238f66ca6313f752b0c9c4691a9061082
Amine42 <amine.bouraghda@gmail.com>
2019-01-14 23:10:03 +0100

    add_ay methode finish

---
+++ lib/06_pig_latin.rb
@@ -0,0 +1,15 @@
+def add_ay(array)
+  array.join("ay ").insert(-1, "ay")
+end
+
+def move_two_first_char(array)
+
+
+end
+
+def translate(str)
+  word = str.split
+  # puts add_ay(word)
+end
+
+puts translate("apple tomate")
