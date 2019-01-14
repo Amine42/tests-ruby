c922081df1db8c940a09628d584b494a39e1be60
Malik42 <malik.ahres@hotmail.fr>
2019-01-14 14:48:23 +0100

    method calculator finish

--- lib/02_calculator.rb
+++ lib/02_calculator.rb
@@ -2,3 +2,31 @@ def add(nb1, nb2)
     nb1 + nb2
 end
 
+def subtract(nb1, nb2)
+    nb1 - nb2
+end
+
+def sum(array)
+    array.sum
+end
+
+def multiply(nb1, nb2)
+    nb1 * nb2
+end
+
+def power(nb1, nb2)
+    (nb1 ** nb2).to_f
+end
+
+def factorial(number)
+    if number == 0 || number == 1
+        number = 1
+    elsif number > 0
+        puts number
+        factorial(number - 1) * number
+    else
+        puts "On ne peut pas calculer une factorielle negatif"
+    end
+end
+
+
