e1f0215195695dd6e64e341227cde25310e7ef90
Malik42 <malik.ahres@hotmail.fr>
2019-01-15 21:59:22 +0100

    correction

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,31 +1,12 @@
-def add_ay(array)
-  array.join("ay ").insert(-1, "ay")
+def translate(phrase)
+	phrase = phrase.split # mais la phrase de l'utilisateur dans un tableau
+	phrase.each_with_index do |word, i| # parcours le tableaux avec la valeur et l'index
+		if word.include? "qu" # si a l'interieur du mots il y a "QU"
+			phrase[i] = word.byteslice(word.index(/qu/)+2,word.length) + word.byteslice(0, word.index(/qu/)+2) + "ay"
+		else
+			phrase[i] = word.byteslice(word.index(/[aeiou]/),word.length) + word.byteslice(0, word.index(/[aeiou]/)) + "ay"
+		end
+	end
+
+	return phrase.join(' ')
 end
-
-def move_two_first_char(array)
-  # if ()
-
-  # puts array
-  real_word = []
-  non_vowel = []
-  array.map.with_index do |word, index|
-    if word[0].upcase != 'A' && word[0].upcase != 'E' && word[0].upcase != 'I' && word[0].upcase != 'O' && word[0].upcase != 'u' && word[0].upcase != 'y'
-      non_vowel[index] = word[0..1]
-      real_word[index] = word.delete(word[0..1]) + non_vowel[index]
-    else
-      real_word[index] = word
-    end
-  end
-  real_word
-
-  # non_vowel_compact = non_vowel.compact!
-  # puts non_vowel_compact
-end
-
-def translate(str)
-  word = str.split
-  
-  add_ay(move_two_first_char(word))
-end
-
-puts translate("the apple tomate the test")
